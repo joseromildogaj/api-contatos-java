8a61a10c0cec3174e6eb55a0f34ca887b3984172
José Romildo <pronuno1@gmail.com>
2022-07-25 02:26:43 -0300

    Criada a classe ContatoService

--- src/main/java/br/com/jr/contatos/service/ContatoService.java
+++ src/main/java/br/com/jr/contatos/service/ContatoService.java
@@ -1,5 +1,49 @@
 package br.com.jr.contatos.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jr.contatos.entity.ContatoEntity;
+import br.com.jr.contatos.repository.ContatoRepository;
+
+@Service
 public class ContatoService {
+
+	@Autowired
+	private ContatoRepository repository;
+	
+
+	public List<ContatoEntity> findAll() {
+		return repository.findAll();
+	}
+	
+
+	public ContatoEntity save(ContatoEntity contato) {
+		return repository.save(contato);
+	}
+	
+
+	public void deleteById(Integer id) {
+		repository.deleteById(id);
+	}
+	
+
+	public void deleteAll() {
+		repository.deleteAll();
+	}
+	
+
+	public Optional<ContatoEntity> findById(Integer id) {
+		return repository.findById(id);
+	}
+	
+
+	public ContatoEntity update(ContatoEntity contato) {
+		return repository.save(contato);
+	}
+	
 
 }
