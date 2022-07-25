0e9d7ef7b4a5fdbd37834c06b71d22c6a1fc3e27
José Romildo <pronuno1@gmail.com>
2022-07-25 01:58:08 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/jr/contatos/ApiContatosJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jr.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContatosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContatosJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jr/contatos/ApiContatosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jr.contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContatosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
