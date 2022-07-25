d9090105eecc31b54b83a1a8f1f78138a96ab56d
José Romildo <pronuno1@gmail.com>
2022-07-25 02:18:06 -0300

    Criada a interface ContatoRepository

--- src/main/java/br/com/jr/contatos/repository/ContatoRepository.java
+++ src/main/java/br/com/jr/contatos/repository/ContatoRepository.java
@@ -1,5 +1,13 @@
 package br.com.jr.contatos.repository;
 
-public class ContatoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jr.contatos.entity.ContatoEntity;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<ContatoEntity, Integer>{
+	
+	
 
 }
